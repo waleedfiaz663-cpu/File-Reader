70ef54110ee14dd76a4d37b42072ba279e490d22
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-09-05 20:44:49 +0500

    Add files via upload

---
+++ FileReader_Test.java
@@ -0,0 +1,34 @@
+
+package file.reader;
+
+import java.io.*;
+
+public class FileReader_Test {
+
+    
+    public static void main(String[] args) {
+       
+        try{
+            
+            FileReader fr = new FileReader ("C:/Users/Administrator/OneDrive/Desktop/Caharcter.txt");
+            
+            int i;
+            
+            while((i=fr.read())!=-1){
+                
+                System.out.println((char)i);
+                
+                
+            }
+            
+            fr.close();
+            
+        }
+        
+        catch(Exception e){
+            
+            System.out.println(e.getMessage());
+        }
+    }
+    
+}
